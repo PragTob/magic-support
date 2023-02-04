a21c45db3de3dbdd8cbb2dffdeeccd4bf57f4947
Tobias Pfeiffer <pragtob@gmail.com>
2023-02-04 13:39:20 +0100

    make API follow pagination links

--- lib/scryfall/client.ex
+++ lib/scryfall/client.ex
@@ -9,14 +9,30 @@ defmodule Scryfall.Client do
   end
 
   def cards_from_set(set_code) do
-    get!("/cards/search",
-      query: [
-        include_extras: true,
-        include_variations: true,
-        order: :set,
-        q: "e:#{set_code}",
-        unique: :prints
-      ]
-    ).body["data"]
+    response =
+      get!("/cards/search",
+        query: [
+          include_extras: true,
+          include_variations: true,
+          order: :set,
+          q: "e:#{set_code}",
+          unique: :prints
+        ]
+      )
+
+    follow_pagination(response.body["data"], response.body["next_page"])
+  end
+
+  defp follow_pagination(data, next_page_url)
+
+  defp follow_pagination(data, nil) do
+    data
+  end
+
+  defp follow_pagination(data, url) do
+    pagination_response = get!(url)
+    more_data = pagination_response.body["data"]
+
+    follow_pagination(data ++ more_data, pagination_response.body["next_page"])
   end
 end
